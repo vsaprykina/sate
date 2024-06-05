09bbcb11706fb896ff7e4e03b78b28047a46fd12
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2024-06-05 23:08:55 +0300

    исправленная версия адаптивности для планшетов

--- site1/main/static/js/opisanie.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var serviceInfos = document.querySelectorAll('.service-info p:not(:last-child)');
-
-  serviceInfos.forEach(function(info) {
-    info.addEventListener('click', function() {
-      this.style.display = 'block';
-    });
-  });
-});
--- site1/main/static/js/stati.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const showMoreButton = document.getElementById("showMoreButton");
-    const hiddenArticles = document.querySelector(".hiddenArticles");
-
-    showMoreButton.addEventListener("click", function() {
-      hiddenArticles.style.display = "grid"; // Показываем скрытые статьи при нажатии на кнопку
-      showMoreButton.style.display = "none"; // Скрываем кнопку после нажатия
-    });
-  });
